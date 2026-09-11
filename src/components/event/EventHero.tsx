336390a4185d8165371759699f1fd7e3b8ecb17f
Skip <skip@usecurling.com>
2026-09-11 14:05:08 -0300

    Atualiza headline principal do hero com destaque e move gancho para subtítulo

--- src/components/event/EventHero.tsx
+++ src/components/event/EventHero.tsx
@@ -60,7 +60,10 @@ export function EventHero() {
             {eventContent.event.name}
           </span>
           <span className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl block font-extrabold text-slate-100 leading-snug">
-            {eventContent.event.headline}
+            <span className="text-[#5BB7D0] font-black">A MELHOR</span>{' '}
+            <span>imersão presencial de</span>{' '}
+            <span className="text-[#5BB7D0] font-black">AUTOGOVERNO EMOCIONAL</span>{' '}
+            <span>do ano</span>
           </span>
         </h1>
 
